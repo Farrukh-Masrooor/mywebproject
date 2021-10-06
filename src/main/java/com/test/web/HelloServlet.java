a007e1ecf240a6d1f2ad640d2e4eebbaedf87c67
farrukh <farrukhmasroor@gmail.com>
2021-10-06 23:40:01 +0530

    added jersey support

--- src/main/java/com/test/web/HelloServlet.java
+++ src/main/java/com/test/web/HelloServlet.java
@@ -1,7 +1,9 @@
 package com.test.web;
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+
+import javax.ws.rs.GET;  
+import javax.ws.rs.Path;  
+import javax.ws.rs.Produces;  
+import javax.ws.rs.core.MediaType; 
 
 @Path("/")
 public class HelloServlet extends HttpServlet {
