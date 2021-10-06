b185aeb28caa16ee3234c6fa0d572b8f01cce8b5
farrukh <farrukhmasroor@gmail.com>
2021-10-06 23:38:57 +0530

    updated helloservlet to support jersey

--- src/main/java/com/test/web/HelloServlet.java
+++ src/main/java/com/test/web/HelloServlet.java
@@ -3,17 +3,12 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+@Path("/")
 public class HelloServlet extends HttpServlet {
-
-  public void doGet(HttpServletRequest request,
-                    HttpServletResponse response)
-            throws ServletException, IOException
-  {
-      // Set response content type
-      response.setContentType("text/html");
-
-      PrintWriter out = response.getWriter();
-      out.println("Hello World from ashar's computer");
-  }
+	
+	@Get
+	public String helloFromJaxRs(){
+	return "hello from jax -rs web services";
+	}
 
 }
