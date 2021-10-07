d0aae9995af9773b81873c027d245db08056d084
farrukh <farrukhmasroor@gmail.com>
2021-10-07 10:47:16 +0530

    updated

--- src/main/java/com/test/web/HelloServlet.java
+++ src/main/java/com/test/web/HelloServlet.java
@@ -1,16 +1,34 @@
 package com.test.web;
-
+/*
 import javax.ws.rs.GET;  
 import javax.ws.rs.Path;  
 import javax.ws.rs.Produces;  
 import javax.ws.rs.core.MediaType; 
 
 @Path("/")
-public class HelloServlet extends HttpServlet {
+public class HelloServlet{
 	
 	@Get
+	@Produces(MediaType.TEXT_HTML)
 	public String helloFromJaxRs(){
 	return "hello from jax -rs web services";
 	}
 
 }
+*/
+
+import javax.ws.rs.GET;  
+import javax.ws.rs.Path;  
+import javax.ws.rs.Produces;  
+import javax.ws.rs.core.MediaType;  
+@Path("/")  
+public class HelloServlet{  
+ 
+  // This method is called if XML is requested  
+  @GET  
+  public String sayXMLHello() {  
+    return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";  
+  }  
+  
+  
+}   
